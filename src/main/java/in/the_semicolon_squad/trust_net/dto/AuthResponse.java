4da09349ccffb773abfc2f1ea545aaf7b0f4c865
ayushgupta674 <ayushgupta674111@gmail.com>
2026-04-04 00:19:23 +0530

    feat: add auth DTOs

---
+++ src/main/java/in/the_semicolon_squad/trust_net/dto/AuthResponse.java
@@ -0,0 +1,10 @@
+package in.the_semicolon_squad.trust_net.dto;
+
+import in.the_semicolon_squad.trust_net.enums.Role;
+
+public class AuthResponse {
+    private String token;
+    private String userId;
+    private String name;
+    private Role role;
+}
